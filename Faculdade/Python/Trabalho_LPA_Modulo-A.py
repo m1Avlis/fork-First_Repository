2f35ca11d939f883c270d9041e0f676396db85b2
m4vlis <omario.pereira96@gmail.com>
2025-03-17 00:11:56 -0300

    feat: Atualalizações na resolução do trabalho de Lógica de Programação

--- Faculdade/Python/Trabalho_LPA_Modulo-A.py
+++ Faculdade/Python/Trabalho_LPA_Modulo-A.py
@@ -87,3 +87,91 @@ while True:
     else: # Reinica o código para um novo pedido.
         continue
 
+#Questão 3
+
+print('Olá! Seja bem vindo a Copiadora do Mario Pereira.')
+
+# Salva o serviço que o usuário deseja.
+def escolha_servico(servicos):
+    while True:
+        global servico
+        servico = input('Escolha o serviço desejado: \n'
+        'DIG - Digitalização \n'
+        'ICO - Impressão Colorida \n'
+        'IPB - Impressão Preto e Branco \n'
+        'FOT - Fotocópia \n'
+        '>> ').upper()
+
+        if servico in servicos:
+            if servico == 'DIG':
+                servico = 1.10
+                break
+            elif servico == 'ICO':
+                servico = 1
+                break
+            elif servico == 'IPB':
+                servico = 0.40
+                break
+            else:
+                servico = 0.20
+                break
+        else:
+            print('Opção inválida, escolha o tipo de serviço novamente.')
+
+# Salva o número de páginas indicado pelo usuário.
+def num_pagina():
+    while True:
+        global pagina
+        try:
+            pagina = int(input('Insira o número de páginas: '))
+            if pagina > 20000:
+                print('Não aceitamos tantas páginas de uma vez')
+            else:
+                break
+        except ValueError:
+            print('Por favor digite apenas números.')
+
+# Pergunta ao usuário se ele deseja algum serviço extra.           
+def servico_extra():
+    while True:
+        global extra
+        try:
+            extra = int(input('Deseja adicionar algum serviço? \n'
+            '1 - Encadernação Simples - R$ 15,00 \n'
+            '2 - Encadernação Capa Dura - R$ R$ 40,00 \n'
+            '0 - Não desejo mais nada.'))
+
+            if extra > 2:
+                print('Opção inválida, digite novamente.')
+            elif extra == 1:
+                extra = 15
+                break
+            elif extra == 2:
+                extra = 40
+                break
+            else:
+                break
+
+        except ValueError:
+            print('Opção inválida, digite novamente.')
+
+
+escolha_servico(['DIG','ICO','IPB','FOT'])
+num_pagina()
+servico_extra()
+
+total = (servico * pagina) + extra
+print(f'Valor Total: R$ {total:.2f}')
+
+
+
+# Questão 4
+# %%
+
+print('Olá! Seja bem vindo a Livraria do Mario Pereira.')
+print('-'*46)
+print('-'*15 + ' MENU PRINCIPAL ' +'-'*15)
+
+lista_livro = []
+id_global = 0
+
--- Faculdade/Python/Treinando_ManipulacaodeStrings.py
+++ Faculdade/Python/Treinando_ManipulacaodeStrings.py
@@ -1,7 +1,14 @@
-texto = 'Hello Word'
+dados = {}
 
-print(texto.capitalize())
-print(texto.lower())
-print(texto.upper())
-print(texto.title())
-print(texto.swapcase())
+while True:
+    frase = input('Digite as frases:')
+    if frase == "":
+        break
+
+    if frase not in dados:
+        dados[frase] = 1
+    else:
+        dados[frase] += 1
+
+for i, j in dados.items():
+    print(i, "->", j)
